a053371520375aeab69b9c5624f9f92ad9c39dca
Bokitza <bokitza@Bokitzas-MacBook-Pro.local>
2020-12-24 09:17:00 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//homeworks!
+
+// 1. fix DRONESOFT into drorsoft. go to w3schools to learn more about deleting and adding elements...
+
+// 2. fix "coding your burekas" to "coding your dreams"
+
+// 3. exchange the background to something more nice when the user is clicking somewhere on the screen...
+//  search for "add event listener" to learn more about "on click" events if you dont know yet
+
+// 4. go to https://jsonplaceholder.typicode.com/ and see the API to download some fake data in json.
+// render it on the screen, and perform miminal styling to it so i would be nice to read! (:
